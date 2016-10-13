c25b41a8011ae6972980a814c616f660ccaca91d
Ilija Boshkov <mindcalamity@gmail.com>
2016-10-13 22:45:58 +0200

    Basic thread view and some refactoring. Definitely needs more of that. Improved thread view responsiveness.

--- resources/views/thread.blade.php
+++ resources/views/thread.blade.php
@@ -30,43 +30,64 @@
             <div class="ui hidden horizontal divider"></div>
 
             <div class="ui ">
-                <div ng-repeat="post in cont.thread.posts" class="ui fluid red card">
+                <div ng-repeat="post in cont.thread.posts" class="ui segment">
+                    <div class="ui grid mobile only">
+                        <div class="equal width row ">
+                            <div class="column">
+                                <div class="ui basic segment">
+                                    <img class="ui tiny left floated image" ng-src="@{{ post.posted_by.profile_img }}"/>
+
+                                    <div class="content">
+                                        <a href="#">@{{ post.posted_by.username }}</a><br/>
+                                        <span class=""><b>Member</b></span><br/>
+                                        <span am-time-ago="post.posted_on"></span><br/>
+                                        <span></span><br/>
+                                        <span></span><br/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="content">
                         <div class="ui divided items">
                             <div class="item">
                                 <div class="ui container equal width grid ">
-                                    <div class="three wide column forum-description">
-                                        <div class="content">
-                                            <div class="left floated ui red card"">
-                                            <div class="image">
-                                                <img class="ui small image" ng-src="@{{ post.posted_by.profile_img }}"/>
-                                            </div>
+                                    <div class="row">
+                                        <div class="three wide column forum-description column tablet only computer and up">
                                             <div class="content">
-                                                <a href="#">@{{ post.posted_by.username }}</a><br/>
-                                                <span class=""><b>Member</b></span><br/>
-                                                <span am-time-ago="post.posted_on"></span>
+                                                <div class="ui">
+
+                                                <div class="ui fluid red card ">
+                                                    <div class="image">
+                                                        <img class="ui small profile image" ng-src="@{{ post.posted_by.profile_img }}"/>
+                                                    </div>
+                                                    <div class="content">
+                                                        <a href="#">@{{ post.posted_by.username }}</a><br/>
+                                                        <span class=""><b>Member</b></span><br/>
+                                                        <span am-time-ago="post.posted_on"></span>
+                                                    </div>
+                                                </div>
+                                                </div>
                                             </div>
                                         </div>
+                                        <div class="column">
+                                            <h3 class="header"><a href="">@{{ post.title }}</a></h3>
 
+                                            <p>Bacon ipsum dolor amet flank jerky burgdoggen pork belly short loin capicola. Ham pork loin kielbasa, landjaeger meatloaf jowl sausage. Chuck boudin jowl pancetta venison ham ball tip leberkas. Fatback short ribs shankle biltong.</p>
+                                            <p>Shank ham hock bresaola, flank t-bone boudin prosciutto. Ball tip fatback flank spare ribs tail. Cow chicken ham pork chop jerky. Salami t-bone ball tip shoulder, pastrami chuck ham hock sirloin ribeye cupim bacon fatback short ribs ham pork chop. Pancetta ham rump cow sirloin tri-tip strip steak. Salami pork loin chicken beef ribs shoulder fatback, spare ribs rump short ribs porchetta. Picanha pork leberkas burgdoggen turkey pig, sirloin shoulder short loin fatback meatloaf pastrami venison t-bone.</p>
+                                        </div>
+                                        </row>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="extra content">
+                                <div class="ui horizontal list">
+                                    <div class="item">
+                                        <button class="ui basic red icon button"><i class="warning sign icon"></i> </button>
                                     </div>
                                 </div>
-                                <div class="column">
-                                    <h3 class="header"><a href="">@{{ post.title }}</a></h3>
-
-                                    <p>Bacon ipsum dolor amet flank jerky burgdoggen pork belly short loin capicola. Ham pork loin kielbasa, landjaeger meatloaf jowl sausage. Chuck boudin jowl pancetta venison ham ball tip leberkas. Fatback short ribs shankle biltong.</p>
-                                    <p>Shank ham hock bresaola, flank t-bone boudin prosciutto. Ball tip fatback flank spare ribs tail. Cow chicken ham pork chop jerky. Salami t-bone ball tip shoulder, pastrami chuck ham hock sirloin ribeye cupim bacon fatback short ribs ham pork chop. Pancetta ham rump cow sirloin tri-tip strip steak. Salami pork loin chicken beef ribs shoulder fatback, spare ribs rump short ribs porchetta. Picanha pork leberkas burgdoggen turkey pig, sirloin shoulder short loin fatback meatloaf pastrami venison t-bone.</p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="extra content">
-                        <div class="ui stackable secondary menu">
-                            <div class="item">
-                                <button class="ui red icon button"><i class="warning sign icon"></i> </button>
-                            </div>
-                            <div class="right menu">
-                                <div class="item">
+                                <div class="ui right floated horizontal list">
                                     <div class="item">
                                         <div class="ui labeled button" tabindex="0">
                                             <div class="ui button">
@@ -74,23 +95,21 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="ui labeled button" tabindex="0">
-                                        <div class="ui green button">
-                                            <i class="thumbs up icon"></i> Say Thanks
+                                    <div class="item">
+                                        <div class="ui labeled button" tabindex="0">
+                                            <div class="ui green button">
+                                                <i class="thumbs up icon"></i> Say Thanks
+                                            </div>
+                                            <a class="ui basic left pointing green label grid column tablet only computer and up">
+                                                1,048
+                                            </a>
                                         </div>
-                                        <a class="ui basic left pointing green label">
-                                            1,048
-                                        </a>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
                 </div>
-            </div>
-        </div>
 
-    </div>
+            </div>
 @endsection
